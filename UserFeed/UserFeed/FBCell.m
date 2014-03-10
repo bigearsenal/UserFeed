e493672455b2fdbbf3e6242ec177864f2cca785c
AaronBratcher <aaronlbratcher@yahoo.com>
2014-03-10 15:47:30 -0400

    Smoother image loading

--- UserFeed/UserFeed/FBCell.m
+++ UserFeed/UserFeed/FBCell.m
@@ -45,56 +45,39 @@
 	self.body.text = wallPost[@"message"];
 	self.image.image = [UIImage imageNamed:@"photoIcon"]; // generic profile image
 
-	if (wallPost[@"picture"]) {
-		NSURL *imageURL = [NSURL URLWithString:wallPost[@"picture"]];
-		//get a dispatch queue
+	// define a block that will load an image from a URL
+	void (^loadImage)(NSString *, UIImageView *) = ^void(NSString *url, UIImageView *view) {
+		NSURL *imageURL = [NSURL URLWithString:url];
 		dispatch_queue_t postImageQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-		//this will start the image loading in bg
 		dispatch_async(postImageQueue, ^{
 			NSData *image = [[NSData alloc] initWithContentsOfURL:imageURL];
+
 			dispatch_async(dispatch_get_main_queue(), ^{
 				if ([from_id isEqualToString:self.from_id]) {
-					self.postImage.image = [UIImage imageWithData:image];
+					view.image = [UIImage imageWithData:image];
 				}
 			});
 		});
+	};
+
+	// load post picture
+	if (wallPost[@"picture"]) {
+		loadImage(wallPost[@"picture"], self.postImage);
 	}
 
+	// load profile picture
 	NSURL *jsonURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?redirect=false&type=normal&width=110&height=110", from_id]];
-	NSData *result = [NSData dataWithContentsOfURL:jsonURL];
-
-	NSDictionary *resultDict = [NSJSONSerialization JSONObjectWithData:result
-	                                                           options:NSJSONReadingMutableContainers
-	                                                             error:NULL];
-	NSURL *imageURL = [NSURL URLWithString:resultDict[@"data"][@"url"]];
-	//get a dispatch queue
-	dispatch_queue_t profileImageQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-	//this will start the image loading in bg
-	dispatch_async(profileImageQueue, ^{
-				NSData *image = [[NSData alloc] initWithContentsOfURL:imageURL];
-				dispatch_async(dispatch_get_main_queue(), ^{
-					if ([from_id isEqualToString:self.from_id]) {
-						self.image.image = [UIImage imageWithData:image];
-					}
-				});
+	dispatch_queue_t profileURLQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+	dispatch_async(profileURLQueue, ^{
+		NSData *result = [NSData dataWithContentsOfURL:jsonURL];
+		if (result) {
+			
+			NSDictionary *resultDict = [NSJSONSerialization JSONObjectWithData:result
+																	   options:NSJSONReadingMutableContainers
+																		 error:NULL];
+			loadImage(resultDict[@"data"][@"url"],self.image);
+		}
 	});
-
-	//	NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
-	//	parameters[@"access_token"] = FBSession.activeSession.accessTokenData;
-	//	parameters[@"width"] = @"64";
-	//	parameters[@"height"] = @"64";
-	//	NSString *path = [NSString stringWithFormat:@"%@/picture", from_id];
-	//
-	//	[self.client getPath:path
-	//	    parameters:parameters
-	//	    success:^(AFHTTPRequestOperation *operation, id responseObject) {
-	//					 if ([from_id isEqualToString:self.from_id]) {
-	//						 self.image.image = [UIImage imageWithData:responseObject];
-	//					 }
-	//		}
-	//	    failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-	//					 NSLog(error.description);
-	//		}];
 
 	[[NSNotificationCenter defaultCenter] addObserver:self
 	                                         selector:@selector(preferredContentSizeChanged:)
